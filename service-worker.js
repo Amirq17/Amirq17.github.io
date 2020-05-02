47418e4d151f1de11ab7f6c8418342ba2b581686
amirqa17 <49957940+amirqa17@users.noreply.github.com>
2020-05-02 20:37:05 +0600

    email fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bfd320ebff728f8785738b6a688411f.js"
+  "/precache-manifest.aa770fe924eb9591dc3bdca7b99f08c9.js"
 );
 
 self.addEventListener('message', (event) => {
