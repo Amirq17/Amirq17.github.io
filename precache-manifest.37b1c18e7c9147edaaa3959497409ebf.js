9118adb193c97cb187bc016decc6cdbbd4ca6b15
amirqa17 <49957940+amirqa17@users.noreply.github.com>
2020-05-03 03:46:36 +0600

    a

---
+++ precache-manifest.37b1c18e7c9147edaaa3959497409ebf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da17b3db536bbbb6367971a89eae60d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "06b49b006c3d9ed669bb",
+    "url": "/test/static/css/2.8261fb29.chunk.css"
+  },
+  {
+    "revision": "66d9f47d76855dead535",
+    "url": "/test/static/css/main.b7cfdbeb.chunk.css"
+  },
+  {
+    "revision": "06b49b006c3d9ed669bb",
+    "url": "/test/static/js/2.b16bee71.chunk.js"
+  },
+  {
+    "revision": "66d9f47d76855dead535",
+    "url": "/test/static/js/main.fe115aea.chunk.js"
+  },
+  {
+    "revision": "28834973d9ad99337da3",
+    "url": "/test/static/js/runtime~main.d79cb7da.js"
+  },
+  {
+    "revision": "e59f6c755b88eb6c1fe8ed6c6917bf31",
+    "url": "/test/static/media/FULL_CV_AMIR.e59f6c75.pdf"
+  },
+  {
+    "revision": "405e2f394d659cd961a7bb7edb915c4f",
+    "url": "/test/static/media/cloer.405e2f39.png"
+  },
+  {
+    "revision": "612785cce1b53fb26f3c2d081ec13725",
+    "url": "/test/static/media/gamestore.612785cc.png"
+  },
+  {
+    "revision": "6aba1cd0b4915e9a52609795f460dba2",
+    "url": "/test/static/media/hospital.6aba1cd0.png"
+  },
+  {
+    "revision": "67f30e6535c33383eabb1fc1c1102aa5",
+    "url": "/test/static/media/movie.67f30e65.mp4"
+  },
+  {
+    "revision": "395892c06c361ba2df0bba0cd1029610",
+    "url": "/test/static/media/pizza.395892c0.png"
+  },
+  {
+    "revision": "dcff2c7a0e474b4c72917f6411c4531c",
+    "url": "/test/static/media/port.dcff2c7a.gif"
+  },
+  {
+    "revision": "8cdefcad6c738bc6ce7bc9b35bc71d80",
+    "url": "/test/static/media/prof.8cdefcad.png"
+  },
+  {
+    "revision": "82a7879442338f76cb0bd255f531ffda",
+    "url": "/test/static/media/qr.82a78794.png"
+  },
+  {
+    "revision": "fe6932b1f82cd4980a665b9db4cddb16",
+    "url": "/test/static/media/sorbent.fe6932b1.png"
+  },
+  {
+    "revision": "96c8fa58140f3b617d82e754c61810e1",
+    "url": "/test/static/media/tizim.96c8fa58.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.509328e67fdd0fdf38d2b6f8cdc22f53.js"
+  "/test/precache-manifest.37b1c18e7c9147edaaa3959497409ebf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
